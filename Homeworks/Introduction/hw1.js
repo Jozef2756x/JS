f092f8cab6b3ff1a1798111dc1039294db34ac26
Jozef2756x <jozefharutunyan@gmail.com>
2025-04-27 10:06:48 -0400

    update2

--- Homeworks/Introduction/hw1.js
+++ Homeworks/Introduction/hw1.js
@@ -1,15 +1,40 @@
-let str = "Chlp";       // Hov jan es mihaty lav chem haskace incha uzum senc em gre
-let num = 16;
-let bool = true;
-let nul = null;
-let undef;
+let str = "Chlp"       // Hov jan es mihaty lav chem haskace incha uzum senc em gre
+let num = 16
+let bool = true
+let nul = null
+let undef
 
-console.log(str, "Type:", typeof str);
+console.log(str, "Type:", typeof str)
 
-console.log(num, "Type:", typeof num);
+console.log(num, "Type:", typeof num)
 
-console.log(bool, "Type:", typeof bool);
+console.log(bool, "Type:", typeof bool)
 
 console.log(nul, "Type:", typeof nul)
 
-console.log(undef, "Type:", typeof undef);
+console.log(undef, "Type:", typeof undef)
+
+let arr1 = [..."banan"]
+let arr2 = [..."nanba"]
+
+let a =[...new Set(arr1)]
+let b =[...new Set(arr1)]
+
+function anagram(a,b){
+    if(a.length !== b.length){
+        console.log("NO")
+        return false
+    }else{
+        a.sort()
+        b.sort()
+        for(let i = 0; i < a.length; ++i){
+            if(a[i] !== b[i]){
+                console.log("NO")
+                return false
+            }
+        }
+        console.log("Yes")
+    }
+}
+
+anagram(a,b)
